e3bc80f36fc5484c8030c976d96d22f2488780aa
gm80474 <gm80474@gmail.com>
2017-06-05 17:00:39 +0800

    002

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send("Hello dev, am a local route nobody knows me yet!1");
+    res.send("Hello dev, am a local route nobody knows me yet!2");
 });
 
 var server = app.listen(port, function () {
